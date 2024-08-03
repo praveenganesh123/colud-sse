e3c754508451703eeab122994f4fd7e056dfaa77
praveenganesh123 <renangipraveenganesh@gmail.com>
2024-08-03 08:58:28 +0530

    v1

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 50, 87, 3000);
+    counter("count2", 150, 201, 2500);
+    counter("count3", 19, 21, 3000);
+
+});
